be1b55f70f4ce8d0e3666e1bb9d5fe973a0d91aa
Dewald <brddew8@protonmail.com>
2024-04-10 21:49:25 +0200

    Added book class & nav menu

--- src/main/java/com/mycompany/librarymanagementsystem/LibraryManagementSystem.java
+++ src/main/java/com/mycompany/librarymanagementsystem/LibraryManagementSystem.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.librarymanagementsystem;
 
+import java.util.Scanner;
+
 /**
  *
  * @author brdde
@@ -11,6 +13,41 @@ package com.mycompany.librarymanagementsystem;
 public class LibraryManagementSystem {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        Scanner input = new Scanner(System.in);
+        int choice;
+        
+        String introMessage = """
+        -------------------------------------------------------------
+                __...--~~~~~-._   _.-~~~~~--...__
+              //    LIBRARY    `V'      BY       \\\\ 
+             //     MANAGER     |  DEWALD BREED   \\\\ 
+            //__...--~~~~~~-._  |  _.-~~~~~~--...__\\\\ 
+           //__.....----~~~~._\\ | /_.~~~~----.....__\\\\
+           ===================\\\\|//====================
+                              `---`
+        -------------------------------------------------------------""";
+     
+        System.out.println(introMessage);
+        System.out.println("What Would You Like To Do?");
+        
+        do {
+            showMenu();
+            choice = input.nextInt();
+        } while (choice != 0);
+    }
+    
+    public static void showMenu(){
+        
+        System.out.println("-------------------------------------------------------------\n"
+                + "Press 0 to Exit \n"
+                + "Press 1 to Add New Books \n"
+                + "Press 2 to View All Books \n"
+                + "Press 3 to Search For Book \n"
+                + "Press 4 to Register New Member \n"
+                + "Press 5 to View Members \n"
+                + "Press 6 to Check Out Book \n"
+                + "Press 7 to Check In Book \n"
+                + "-------------------------------------------------------------");
     }
 }
